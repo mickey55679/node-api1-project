5d00669738d18c39abb7d3bf48c563c024149164
Michaiah Bos <michaiahbos@yahoo.com>
2024-02-17 13:28:21 -0600

    wip

--- api/server.js
+++ api/server.js
@@ -1,3 +1,13 @@
 // BUILD YOUR SERVER HERE
+// global obejcet and whatever we set it to be becomes required in other modules 
+const express = require('express')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
