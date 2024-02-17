8ba6d86921552324a1c6cf423def362eed471dfc
Michaiah Bos <michaiahbos@yahoo.com>
2024-02-17 14:00:59 -0600

    first two tests

--- api/server.js
+++ api/server.js
@@ -1,11 +1,22 @@
 // BUILD YOUR SERVER HERE
 // global obeject and whatever we set it to be becomes required in other modules 
 const express = require('express')
-
+const User = require('./users/model')
 const server = express()
 
 server.get('/api/users', (req, res) => {
-    res.json('users')
+    User.find()
+    .then(users => {
+      res.json(users)
+
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting users',
+            err: err.message,
+            stack: err.stack,
+        })
+    })
 })
 
 server.use('*', (req, res) => {
